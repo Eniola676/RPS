6a11aad255cdd7e7aef44933ff9c60e77c0f5e4f
Eniola676 <ojetoyinbotobi@gmail.com>
2022-07-19 14:34:46 +0100

    Project roundup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter as Router,Routes,Route} from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom";
 
 import './styles.scss';
 import App from './App';
